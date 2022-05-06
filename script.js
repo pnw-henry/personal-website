cc4b0d68dd28624694afa81088ffa1b46359f60d
Henry Escobar <henrye@gmail.com>
2022-05-06 04:24:02 -0700

    Changed overall look

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const h3 = document.createElement("h3");
-h3.textContent = "In the meantime, go to my Instagram for pretty photos.";
-h3.className = "dynamic-text";
+const h4 = document.createElement("h4");
+h4.textContent = "In the meantime, go to my Instagram for pretty photos.";
+h4.className = "dynamic-text";
 const input = document.getElementById("dyamic-text");
 
-document.querySelector("body").appendChild(h3);
+document.querySelector("body").appendChild(h4);

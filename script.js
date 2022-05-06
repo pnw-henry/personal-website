bd303400d72d1524bbbdeeff0c28412507d396a4
Henry Escobar <henrye@gmail.com>
2022-05-06 03:29:45 -0700

    modified script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h3 = document.createElement("h2");
+const h3 = document.createElement("h3");
 h3.textContent = "In the meantime, go to my Instagram for pretty photos.";
 h3.className = "dynamic-text";
 const input = document.getElementById("dyamic-text");
